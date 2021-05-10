952e480a06671aba296e3b5d80aa63aeb148a622
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2021-05-10 08:50:21 -0300

    Fixed errors
    Fixed wrong state and changed the app class name.

--- simple_personality/lib/main.dart
+++ simple_personality/lib/main.dart
@@ -5,21 +5,21 @@ import './quiz.dart';
 import './result.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SimplePersonality());
 }
 
-class MyApp extends StatelessWidget {
+class SimplePersonality extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return SimplePersonalityState();
   }
 }
 
-class MyAppState extends StatefulWidget {
-  var _questions = const[
+class SimplePersonalityState extends State<SimplePersonality> {
+  var _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers':[
+      'answers': [
         {'text': 'Black', 'score': 10},
         {'text': 'Red', 'score': 5},
         {'text': 'Green', 'score': 3},
@@ -49,21 +49,21 @@ class MyAppState extends StatefulWidget {
   var _questionIndex = 0;
   var _totalScore = 0;
 
-  void _resetQuiz(){
+  void _resetQuiz() {
     setState(() {
       _questionIndex = 0;
       _totalScore = 0;
     });
   }
 
-  void _answerQuestion(int score){
+  void _answerQuestion(int score) {
     _totalScore += score;
 
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < _questions.length){
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -71,19 +71,20 @@ class MyAppState extends StatefulWidget {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Simple Personality App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
-      body: _questionIndex < _questions.length ? Quiz(
-        answerQuestion: _answerQuestion,
-        questionIndex: _questionIndex,
-        questions: _questions,
-      )
-      : Result(_totalScore, _resetQuiz),
-    ),
-  };
-}
+    );
+  }
 }
